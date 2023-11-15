500258329ae8ccd59d94cb9ce5e3e8f75293c48e
Shinwoo Kim <shinwoo.k0370@gmail.com>
2023-11-15 12:55:33 -0500

    add sample results page

--- server.py
+++ server.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("home.html")
+
+
+@app.route("/results")
+def sample_results():
+    return render_template("results.html")
