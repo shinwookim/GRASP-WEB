b98adb4eb91378ecb3fedb196b2c81d3ec69945e
Zi Han Ding <zid8@pitt.edu>
2023-11-30 18:43:53 -0500

    uploads now work and will create an upload directory at app/uploads, same thing for downloads

--- app/models/result.py
+++ app/models/result.py
@@ -3,7 +3,8 @@ from datetime import datetime
 
 class Result(db.Model):
     id = db.Column(db.Integer, primary_key = True) # Primary key for the result
-    filename = db.Column(db.String(64), index = True, unique = True) # Filename of the result
+    input_filename = db.Column(db.String(64), index = True, unique = True) # Filename of the result
+    result_filename = db.Column(db.String(64), index = True, unique = True) # Filename of the result
     result = db.Column(db.String(120)) # Result of the experiment
     timestamp = db.Column(db.DateTime, index = True, default = datetime.utcnow) # Timestamp of the result
 
--- config.py
+++ config.py
@@ -7,5 +7,5 @@ class Config():
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')\
         or 'sqlite:///' + os.path.join(basedir, 'app.db') # get the database url from the environment variable, or use the default sqlite database
     SQLALCHEMY_TRACK_MODIFICATIONS = False # disable the modification tracker for the database
-    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER') or "./uploads" # get the upload folder from the environment variable, or use the default uploads folder
-    DOWNLOAD_FOLDER = os.environ.get('DOWNLOAD_FOLDER') or "./downloads" # get the download folder from the environment variable, or use the default downloads folder
+    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER') # get the upload folder from the environment variable, or use the default uploads folder
+    DOWNLOAD_FOLDER = os.environ.get('DOWNLOAD_FOLDER') # get the download folder from the environment variable, or use the default downloads folder
