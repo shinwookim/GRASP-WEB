e2d7839d2f389a869ea19d23ab40ffe9e30f7cac
Zi Han Ding <zid8@pitt.edu>
2023-12-07 13:04:31 -0500

    matplotlib functions imported initially

--- app/models/result.py
+++ app/models/result.py
@@ -5,7 +5,6 @@ class Result(db.Model):
     id = db.Column(db.Integer, primary_key = True) # Primary key for the result
     input_filename = db.Column(db.String(64), index = True, unique = True) # Filename of the result
     result_filename = db.Column(db.String(64), index = True, unique = True) # Filename of the result
-    result = db.Column(db.String(120)) # Result of the experiment
     timestamp = db.Column(db.DateTime, index = True, default = datetime.utcnow) # Timestamp of the result
 
     def __repr__(self):
